1eaea06183f4680eed1f7e4a9f3ac902da68ffba
Jason Tucker <jtucker@strategicmarketing.com>
2024-11-11 18:40:23 -0500

    Renamed HelloWorld to SelectUser

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import SelectUser from './components/SelectUser.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <SelectUser />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/SelectUser.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useMainStore } from "../stores/main.ts";
+
+const store = useMainStore();
+
+const selectUser = () => {}
+</script>
+
+<template>
+  <div class="card">
+    <button v-for="user in store.users" type="button" @click="selectUser">{{ user }}</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
